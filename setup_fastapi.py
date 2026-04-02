6dd746ca063844b202ef7954fc16897c17d51158
Hasib303 <hasib303bd@gmail.com>
2026-04-02 16:32:22 +0600

    Add patient data and API endpoints for viewing patient records

--- setup_fastapi.py
+++ setup_fastapi.py
@@ -1,11 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+import json
 
 app = FastAPI() # app is object
+
+def get_data():
+    with open('patients.json', 'r') as file:
+        return json.load(file)
 
 @app.get("/")
 def hello():
     return { 'message':'Hello world'}
 
+@app.get("/view")
+def view():
+    data = get_data()
+    return data
+
+@app.get('/view/{patient_id}')
+def view_patient(patient_id: str = Path(..., description='The ID of the patient to view')):
+    data = get_data()
+    for patient in data['records']:
+        if patient['patient_id'] == patient_id:
+            return patient
+    return {'error':'Patient not found'}
 
 @app.get('/about')
 def about():

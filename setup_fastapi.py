c31254d39e2b70862026fabc19fb598baa8e1791
Hasib303 <hasib303bd@gmail.com>
2026-04-03 01:58:24 +0600

    Add height, weight, and BMI to patient records; implement sorting functionality for patient data retrieval

--- setup_fastapi.py
+++ setup_fastapi.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 
 app = FastAPI() # app is object
@@ -24,6 +24,20 @@ def view_patient(patient_id: str = Path(..., description='The ID of the patient 
             return patient
         raise HTTPException(status_code=404, detail='Patient not found')
 
+@app.get('/patient')
+def sort_patients(sort_by: str = Query(..., description='The field to sort by'), order: str = Query(..., description='The order to sort by')):
+    valid_fields = ['height_cm', 'weight_kg', 'bmi']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail='Invalid sort field')
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code=400, detail='Invalid order')
+
+    data = get_data()
+    sort_order = True if order == 'asc' else False
+    sorted_data = sorted(data['records'], key=lambda x: x[sort_by], reverse=sort_order)
+
+    return sorted_data
 
 @app.get('/about')
 def about():

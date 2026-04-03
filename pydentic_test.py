a13922fb7054a84e219ef0b29bc35c968dfdcd80
Hasib303 <hasib303bd@gmail.com>
2026-04-04 01:14:56 +0600

    Refactor insert_patient function to accept patient object; add update_patient_data function for displaying patient info, till now these are type validation

--- pydentic_test.py
+++ pydentic_test.py
@@ -5,9 +5,18 @@ class patient(BaseModel):
     name: str
     age: int
 
-def insert_patient(name, age):
+def insert_patient(patient: patient):
+    print(patient.name)
+    print(patient.age)
 
-    print(name)
-    print(age)
 
+def update_patient_data(patient: patient):
+    print(patient.name)
+    print(patient.age)
 
+patient_info = {"name": "John Doe", "age": 25}
+
+patient1 = patient(**patient_info)
+
+# insert_patient(patient1)
+update_patient_data(patient1)

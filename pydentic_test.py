2abaa918f8c88213e7b8a9b6936960d3319cf5ba
Hasib303 <hasib303bd@gmail.com>
2026-04-05 15:59:19 +0600

    Enhance patient model in pydentic_test.py by adding weight, allergies, contact details, email, and optional LinkedIn URL fields; update patient_info dictionary accordingly. Add email-validator dependency in pyproject.toml for email validation.

--- pydentic_test.py
+++ pydentic_test.py
@@ -1,9 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, HttpUrl, Field
+from typing import List, Dict, Optional
 
 
 class patient(BaseModel):
-    name: str
-    age: int
+    name: str = Field(..., min_length=3, max_length=100)
+    age: int = Field(..., ge=0, le=120)
+    weight: float
+    allergies: list[str]
+    contact_details: dict[str, str]
+    email: EmailStr
+    linkedin_url: Optional[HttpUrl] = None
 
 def insert_patient(patient: patient):
     print(patient.name)
@@ -13,8 +19,13 @@ def insert_patient(patient: patient):
 def update_patient_data(patient: patient):
     print(patient.name)
     print(patient.age)
+    print(patient.weight)
+    print(patient.allergies)
+    print(patient.contact_details)
+    print(patient.email)
+    print(patient.linkedin_url)
 
-patient_info = {"name": "John Doe", "age": 25}
+patient_info = {"name": "John", "age": 120, "weight": 70.5, "allergies": ["penicillin", "shellfish"], "contact_details": {"email": "john.doe@example.com", "phone": "123-456-7890"}, "email": "john.doe@example.com", "linkedin_url": "http://linkedin.com/in/john-doe-1234567890"}
 
 patient1 = patient(**patient_info)
 
